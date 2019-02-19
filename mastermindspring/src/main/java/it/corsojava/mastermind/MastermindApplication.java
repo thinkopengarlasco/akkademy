513add38cac303ab4d6893d0507199ea99c2418d
Raffaella Zaza <r.zaza6@studenti.uniba.it>
2019-02-19 15:19:08 +0100

    remove linea commentata

--- mastermindspring/src/main/java/it/corsojava/mastermind/MastermindApplication.java
+++ mastermindspring/src/main/java/it/corsojava/mastermind/MastermindApplication.java
@@ -2,7 +2,8 @@ package it.corsojava.mastermind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//example
+
+
 @SpringBootApplication
 public class MastermindApplication {
 
